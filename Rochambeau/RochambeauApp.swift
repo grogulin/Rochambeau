94c9a082069faaf2de184f3ffa8cbadaeadcac08
Yaroslav Grogul <time7bass@gmail.com>
2022-12-20 22:14:50 +0100

    Initial Commit

---
+++ Rochambeau/RochambeauApp.swift
@@ -0,0 +1,17 @@
+//
+//  RochambeauApp.swift
+//  Rochambeau
+//
+//  Created by Ярослав Грогуль on 20.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct RochambeauApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
